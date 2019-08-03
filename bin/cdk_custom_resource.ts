0be1dafe3a0d55b5736e763a597c84c7f4b7a801
HIRAUCHI <hirauchi.shinichi@classmethod.jp>
2019-08-03 10:55:23 +0900

    first commit

--- bin/cdk_custom_resource.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { CdkCustomResourceStack } from '../lib/cdk_custom_resource-stack';
-
-const app = new cdk.App();
-new CdkCustomResourceStack(app, 'CdkCustomResourceStack');
